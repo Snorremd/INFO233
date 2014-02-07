3abbf5b5e01c56daf85e249f5758d280913d8f8c
Haakon Løtveit <haakon_lotveit@live.no>
2014-02-07 13:30:46 +0100

    Siste finpuss

--- Obligatorisk Oppgave 1/info233 oblig1 v14/src/exceptions/TwitterConnectionException.java
+++ Obligatorisk Oppgave 1/info233 oblig1 v14/src/exceptions/TwitterConnectionException.java
@@ -2,4 +2,9 @@ package exceptions;
 
 public class TwitterConnectionException extends Exception {
 
+	/**
+	 * Autogenerert
+	 */
+	private static final long serialVersionUID = -7018565699633457378L;
+
 }
--- Obligatorisk Oppgave 1/info233 oblig1 v14/src/io/TwitterStream.java
+++ Obligatorisk Oppgave 1/info233 oblig1 v14/src/io/TwitterStream.java
@@ -5,7 +5,6 @@
  */
 package io;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
