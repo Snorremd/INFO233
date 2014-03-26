c089508d61861dd540e6af6e4a48ff4a765cdc60
Haakon Løtveit <haakon_lotveit@live.no>
2014-03-26 19:16:39 +0100

    Mer samlecommits

---
+++ Oblig2-kladding/Kjips Kylling/src/game/entity/monster/ExampleMonster.java
@@ -0,0 +1,64 @@
+package game.entity.monster;
+
+import game.entity.types.Level;
+import game.util.Direction;
+import game.util.Mover;
+import game.view.gfx.SpriteLoader;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Et eksempel på et enkelt monster.
+ * Går en tilfeldig rute per tick.
+ * 
+ * @author Haakon Løtveit (haakon.lotveit@student.uib.no)
+ *
+ */
+public class ExampleMonster extends AbstractMonster {
+	private int tickCounter = 0;
+	/**
+	 * 
+	 * @param level brettet monsteret skal være på
+	 * @param loader SpriteLoaderen som kan gi monsteret tilgang til spritesene sine.
+	 * @param column Kolonnen (X-verdien) som monsteret skal begynne på.
+	 * @param row Raden (Y-verdien) som monsteret skal begynne på.
+	 */
+	public ExampleMonster(Level level, SpriteLoader loader, int column, int row){
+		/*
+		 * Vi bruker altså den øverste raden i spritesheeten monstre
+		 */
+		super(column, row, level, loader, 0);
+	}
+
+	@Override
+	public void tick() {
+		/* Gå tilfeldig retning hver tick 44. tick
+		 * Dere bør gi deres monstre mer underholdende kommandoer. */
+
+		
+		/* Bare gjør noe hver nte tick. */
+		int nteTick = 44;
+		if((tickCounter++ % nteTick) != 0){
+			return;
+		}
+		
+		/* Gå i en tilfeldig retning */
+		Direction[] directions = Direction.values();
+		ArrayList<Direction> placesWeCanGo = new ArrayList<>(4);
+		for(Direction d : directions){
+			int[] pos = Mover.position(d, getColumn(), getRow());
+			if(level.walkable(pos[Mover.COLUMN], pos[Mover.ROW])){
+				placesWeCanGo.add(d);
+			}
+		}
+		Direction walkDir = placesWeCanGo.get(new Random().nextInt(placesWeCanGo.size()));
+		this.move(walkDir, level);
+	}
+
+	
+	@Override
+	public byte getPriority(){
+		return (byte) 0;
+	}
+}
